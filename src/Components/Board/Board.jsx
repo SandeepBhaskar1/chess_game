647e238b75d904bf01f793cf54cb70f088716ea2
Sandeep Bhaskar <ssandeepachari1@gmail.com>
2024-12-22 20:29:59 +0530

    check done

--- src/Components/Board/Board.jsx
+++ src/Components/Board/Board.jsx
@@ -5,6 +5,8 @@ import Files from './bits/Files';
 import Pieces from '../Pieces/Pieces';
 import { useAppContext } from '../../contexts/Context';
 import Popup from '../Popup/Popup';
+import arbiter from '../../arbiter/arbiter';
+import { getKingPosition } from '../../arbiter/getMoves';
 
 const Board = () => {
   const ranks = Array(8).fill().map((x, i) => 8 - i);
@@ -13,18 +15,30 @@ const Board = () => {
   const { appState } = useAppContext();
   const position = appState.position[appState.position.length - 1];
 
+  const isChecked =(() => {
+    const isInCheck = arbiter.isPlayerInCheck({
+      positionAfterMove: position,
+      player : appState.turn
+    })
+    if (isInCheck)
+      return getKingPosition(position,appState.turn)
+    return null;
+  })()
+
   const getClassName = (i, j) => {
     let c = 'tile';
     c += (i + j) % 2 === 0 ? ' dark--tile' : ' light--tile';
-
+  
     if (appState.candidateMoves?.some(m => m[0] === i && m[1] === j)) {
-      if (position [i] [j])
-        c += ' attacking'
-      else
-        c += ' highlight' 
+      if (position[i][j]) c += ' attacking';
+      else c += ' highlight';
     }
-    return c
+  
+    if (isChecked && isChecked[0] === i && isChecked[1] === j) c += ' checked';
+  
+    return c;
   };
+  
 
   return (
     <div className="board">
--- src/arbiter/arbiter.jsx
+++ src/arbiter/arbiter.jsx
@@ -1,53 +1,89 @@
-import { getBishopMoves, getKingMoves, getKnightMoves, getQueenMoves, getRookMoves, getSoldiersCapture, getSoldiersMoves, getCastlingMoves } from "./getMoves"
+import { getBishopMoves, getKingMoves, getKnightMoves, getQueenMoves, getRookMoves, getSoldiersCapture, getSoldiersMoves, getCastlingMoves, getKingPosition, getPieces } from "./getMoves"
 import { movePawn, movePiece } from "./move";
 
 const arbiter = {
-    getRegularMoves : function ({position, previousPosition, piece, rank, file}) {
+    getRegularMoves: function ({ position, previousPosition, piece, rank, file }) {
         if (piece.endsWith('n'))
-            return getKnightMoves({position, rank, file});
+            return getKnightMoves({ position, rank, file });
 
         if (piece.endsWith('b'))
-            return getBishopMoves({position, piece, rank, file});
+            return getBishopMoves({ position, piece, rank, file });
 
         if (piece.endsWith('q'))
-            return getQueenMoves({position, piece, rank, file});
+            return getQueenMoves({ position, piece, rank, file });
 
         if (piece.endsWith('k'))
-            return getKingMoves({position, piece, rank, file});
+            return getKingMoves({ position, piece, rank, file });
 
         if (piece.endsWith('r'))
-        return getRookMoves({position, piece, rank, file});
+            return getRookMoves({ position, piece, rank, file });
 
         if (piece.endsWith('p'))
-        return getSoldiersMoves({position, piece, rank, file});
-            
-        },
+            return getSoldiersMoves({ position, piece, rank, file });
 
-        getValidMoves : function ({position, previousPosition, castleDirection, piece, rank, file}) {
-            let moves = this.getRegularMoves({position, piece, rank, file})
-            if ( piece.endsWith('p')) {
-                moves =[
-                    ...moves,
-                    ...getSoldiersCapture ({position, previousPosition, piece, rank, file})
-                ]
-            }
-            if ( piece.endsWith('k')) {
-                moves =[
-                    ...moves,
-                    ...getCastlingMoves ({position, castleDirection, piece, rank, file})
-                ]
-            }
-            return moves
-        },
+        return [];
+    },
 
-        performMove: function ({ position, piece, rank, file, x, y }) {
-            if (piece.endsWith('p')) {
-                return movePawn({ position, piece, rank, file, x, y });
-            } else if (piece) {
-                return movePiece({ position, piece, rank, file, x, y });
-            } 
+    getValidMoves: function ({ position, previousPosition, castleDirection, piece, rank, file }) {
+        let moves = this.getRegularMoves({ position, piece, rank, file });
+        const notInCheckMoves = [];
+
+        if (piece.endsWith('p')) {
+            moves = [
+                ...moves,
+                ...getSoldiersCapture({ position, previousPosition, piece, rank, file })
+            ];
         }
+
+        if (piece.endsWith('k')) {
+            moves = [
+                ...moves,
+                ...getCastlingMoves({ position, castleDirection, piece, rank, file })
+            ];
+        }
+
+        moves.forEach(([x, y]) => {
+            const positionAfterMove = this.performMove({ position, piece, rank, file, x, y });
+
+            if (!this.isPlayerInCheck({ positionAfterMove, position, player: piece[0] }))
+                notInCheckMoves.push([x, y]);
+        });
+
+        return notInCheckMoves;
+    },
+
+    performMove: function ({ position, piece, rank, file, x, y }) {
+        if (piece.endsWith('p')) {
+            return movePawn({ position, piece, rank, file, x, y });
+        } else if (piece) {
+            return movePiece({ position, piece, rank, file, x, y });
+        }
+    },
+
+    isPlayerInCheck: function ({ positionAfterMove, position, player }) {
+        const enemy = player.startsWith('w') ? 'b' : 'w';
+        const kingPos = getKingPosition(positionAfterMove, player);
+        const enemyPieces = getPieces(positionAfterMove, enemy);
+
+        const enemyMoves = enemyPieces.reduce((acc, p) => acc = [
+            ...acc,
+            ...(p.piece.endsWith('p'))
+                ? getSoldiersCapture({
+                    position: positionAfterMove,
+                    previousPosition: position,
+                    ...p
+                })
+                : this.getRegularMoves({
+                    position: positionAfterMove,
+                    ...p
+                })
+        ], []);
+
+        if (enemyMoves.some(([x, y]) => kingPos[0] === x && kingPos[1] === y))
+            return true;
+
+        return false;
     }
-        
+}
 
 export default arbiter;
